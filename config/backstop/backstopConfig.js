6a8b3acf16d1cbc09af02b5604507fb814144f08
gestap <gestap9@gmail.com>
2019-07-25 11:54:11 +0300

    added module backstopConfig and script files

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -1,27 +1,20 @@
 'use strict';
 
 // https://github.com/garris/BackstopJS#advanced-scenarios
-const basicScenario = {
-  label: 'search-bar-Airbnb', // name of the test
-  url: 'http://localhost:8080/index.html',
-  // eslint-disable-next-line max-len
-  referenceUrl: 'https://mate-academy.github.io/layout_solutions/search-bar-airbnb/',
-  readyEvent: '',
-  readySelector: '',
-  delay: 1000,
-  hideSelectors: ['[data-qa-hidden]'],
-  removeSelectors: ['[data-qa-remove]'],
-  hoverSelector: '',
-  clickSelector: '',
-  postInteractionWait: 0,
-  selectors: '',
-  selectorExpansion: true,
-  expect: 0,
-  misMatchThreshold: 1, // 1% of 100%
+
+const backstop = require('@mate-academy/backstop-config');
+
+const { basicScenario } = backstop;
+
+const basic = {
+  ...basicScenario,
+  label: 'Elementary test',
+  referenceUrl:
+    'https://mate-academy.github.io/layout_solutions/search-bar-airbnb/',
 };
 
-module.exports = {
-  id: 'test',
+const config = {
+  ...backstop,
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
   viewports: [
@@ -38,17 +31,17 @@ module.exports = {
   ],
   scenarios: [
     {
-      ...basicScenario,
+      ...basic,
       label: 'big-search-bar',
       selectors: ['[data-qa="big"]'],
     },
     {
-      ...basicScenario,
+      ...basic,
       label: 'small-search-bar',
       selectors: ['[data-qa="small"]'],
     },
     {
-      ...basicScenario,
+      ...basic,
       label: 'key-press',
       selectors: ['[data-qa="big"]'],
       keyPressSelectors: [
@@ -59,20 +52,6 @@ module.exports = {
       ],
     },
   ],
-  paths: {
-    bitmaps_reference: 'backstop_data/bitmaps_reference',
-    bitmaps_test: 'backstop_data/bitmaps_test',
-    engine_scripts: 'backstop_data/engine_scripts',
-    html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report',
-  },
-  report: ['browser'],
-  engine: 'puppeteer',
-  engineOptions: {
-    args: ['--no-sandbox'],
-  },
-  asyncCaptureLimit: 5,
-  asyncCompareLimit: 50,
-  debug: false,
-  debugWindow: false,
 };
+
+module.exports = config;

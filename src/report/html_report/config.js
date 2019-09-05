9ed7e1c0f834b02782a63a11db2fcca073794a6d
Shurkodr <sashaglazunov2@gmail.com>
2019-09-05 13:46:31 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,161 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1862308459_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "label": "big-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -24,
+            "height": 0
+          },
+          "misMatchPercentage": "3.35",
+          "analysisTime": 38
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_big-search-bar_0_data-qabig_0_desktop_s.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1862308459_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "label": "big-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -24,
+            "height": 0
+          },
+          "misMatchPercentage": "3.61",
+          "analysisTime": 67
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_big-search-bar_0_data-qabig_1_desktop_xs.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "1862308459_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "label": "small-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -24,
+            "height": -2
+          },
+          "misMatchPercentage": "4.53",
+          "analysisTime": 30
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_small-search-bar_0_data-qasmall_0_desktop_s.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "1862308459_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "label": "small-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 24,
+            "height": 2
+          },
+          "misMatchPercentage": "5.43",
+          "analysisTime": 26
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_small-search-bar_0_data-qasmall_1_desktop_xs.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_key-press_0_data-qabig_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_key-press_0_data-qabig_0_desktop_s.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1862308459_key-press_0_data-qabig_0_desktop_s.png",
+        "label": "key-press",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -24,
+            "height": 0
+          },
+          "misMatchPercentage": "2.46",
+          "analysisTime": 25
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_key-press_0_data-qabig_0_desktop_s.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1862308459_key-press_0_data-qabig_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190905-133134\\1862308459_key-press_0_data-qabig_1_desktop_xs.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1862308459_key-press_0_data-qabig_1_desktop_xs.png",
+        "label": "key-press",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -24,
+            "height": 0
+          },
+          "misMatchPercentage": "2.88",
+          "analysisTime": 28
+        },
+        "diffImage": "..\\bitmaps_test\\20190905-133134\\failed_diff_1862308459_key-press_0_data-qabig_1_desktop_xs.png"
+      },
+      "status": "fail"
+    }
+  ]
+});

b9c0de07ed8ccd30ba70a87d64b1ddf8351b1dbd
youvovk <wer3wollf@gmail.com>
2019-09-07 18:06:59 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,155 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1146586126_big-search-bar_0_data-qabig_0_desktop_s.png",
+        "label": "big-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.00",
+          "analysisTime": 83
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1146586126_big-search-bar_0_data-qabig_1_desktop_xs.png",
+        "label": "big-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.99",
+          "analysisTime": 64
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "1146586126_small-search-bar_0_data-qasmall_0_desktop_s.png",
+        "label": "small-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.65",
+          "analysisTime": 48
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "1146586126_small-search-bar_0_data-qasmall_1_desktop_xs.png",
+        "label": "small-search-bar",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.93",
+          "analysisTime": 41
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_key-press_0_data-qabig_0_desktop_s.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_key-press_0_data-qabig_0_desktop_s.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1146586126_key-press_0_data-qabig_0_desktop_s.png",
+        "label": "key-press",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_s",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.66",
+          "analysisTime": 49
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1146586126_key-press_0_data-qabig_1_desktop_xs.png",
+        "test": "..\\bitmaps_test\\20190907-180557\\1146586126_key-press_0_data-qabig_1_desktop_xs.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "1146586126_key-press_0_data-qabig_1_desktop_xs.png",
+        "label": "key-press",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "desktop_xs",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.94",
+          "analysisTime": 40
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});

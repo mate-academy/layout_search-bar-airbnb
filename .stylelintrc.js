791cb266c83df161a5ab62f2b607ae0e98ae42d3
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2023-03-30 20:44:42 +0300

    add lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   extends: [
-    "stylelint-config-recommended-scss",
+    "stylelint-config-standard-scss",
     "@mate-academy/stylelint-config"
-  ],
-  plugins: [
-    "stylelint-scss"
   ],
   rules: {}
 };
+
+
